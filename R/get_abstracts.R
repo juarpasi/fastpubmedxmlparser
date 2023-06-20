ba43d32ad70704ceeda95dd8d78631204ec036d9
juarpasi <juarpasi97@gmail.com>
2023-06-20 14:46:27 -0600

    added fun to get abstracts

---
+++ R/get_abstracts.R
@@ -0,0 +1,36 @@
+#' Extract the abstracts with associated data from one PubMed record
+#'
+#' @param doc str. An PubMed xml record of one record.
+#'
+#' @return df. A table with the text, label and Nlm category
+#' @export
+#'
+#' @examples
+#'
+#' system.file("extdata", "one_record_male_cancer01.txt", package = "fastpubmedxmlparser") |>
+#'   readPubmedFiles() |>
+#'   get_abstract()
+
+get_abstract <- function(doc) {
+
+  #get nodes
+  nodes <- doc |>
+    xml2::read_xml() |>
+    xml2::xml_find_all('.//MedlineCitation//Article//Abstract//AbstractText')
+
+  if (length(nodes) == 0) return(NULL)
+
+  #get text
+  df <- data.frame(text = xml2::xml_text(nodes))
+
+  attribs <- c('Label','NlmCategory')
+
+  #get value of attr and add to dataframe
+  for (att in attribs) {
+    df[att] <- xml2::xml_attr(nodes, attr = att)
+  }
+
+  nodes <- NULL
+
+  return(df)
+}
